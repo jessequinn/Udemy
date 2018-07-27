df13b17b9b8a18197cbf56014ee5450e37da1028
jessequinn <me@jessequinn.info>
2018-07-27 11:57:15 -0300

    folder changes, and upto python Seciton 7, Lecture 39

---
+++ Complete_Python_Masterclass/Examples/aa.py
@@ -0,0 +1,22 @@
+# # augmented assignment
+# number = "9,223,372,036,854,775,807"
+# cleanedNumber = ''
+
+# for i in range(0, len(number)):
+#     if number[i] in '0123456789':
+#         cleanedNumber += number[i]
+
+# newNumber = int(cleanedNumber)
+
+# print("The nubmer is {} ".format(newNumber))
+
+
+number = 5
+multiplier = 8
+answer = 0
+
+# add your loop after this comment
+
+for i in range(1,(multiplier+1)):
+    answer += number
+print(answer)
