5608ee054605dab470a8b39cfe6918a57ea38f9a
jessequinn <me@jessequinn.info>
2018-08-19 18:15:15 -0300

    update to lesson 99.

--- Complete_Python_Masterclass/Source_Codes/lesson_95_functions.py
+++ Complete_Python_Masterclass/Source_Codes/lesson_95_functions.py
@@ -4,13 +4,12 @@ def python_food():
     left_margin = (width - len(text)) // 2
     print(" " * left_margin, text)
 
-python_food()
-
-def centre_text(text):
-    text = str(text)
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
-
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
 # call the function
 centre_text("spam and eggs")
@@ -18,4 +17,5 @@ centre_text("spam, spam and eggs")
 centre_text(12)
 centre_text("spam, spam, spam and spam")
 
-print("first", "second", 3, 4, "spam")
+centre_text("first", "second", 3, 4, "spam", sep=":")
+print()
