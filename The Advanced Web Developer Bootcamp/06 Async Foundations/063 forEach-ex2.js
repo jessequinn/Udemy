e3d652c8144660ae155149d897b6364416106c7c
Jesse Quinn <me@jessequinn.info>
2018-10-26 19:50:26 -0300

    modified example

--- The Advanced Web Developer Bootcamp/06 Async Foundations/063 forEach-ex2.js
+++ The Advanced Web Developer Bootcamp/06 Async Foundations/063 forEach-ex2.js
@@ -1,5 +1,6 @@
- var arr = [1,2,3,4,5,6];
- forEach(arr, function(number) {
-     console.log(number * 2);
- });
+let arr = [1, 2, 3, 4, 5, 6];
+
+forEach(arr, function (number) {
+    console.log(number * 2);
+});
 
